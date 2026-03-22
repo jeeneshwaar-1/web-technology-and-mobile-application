42770eba66d5ce6d17425df7cdc57b6f7e671efe
jeeneshwaar-1 <jeeneshwaar.s.2024.aiml@rajalakshmi.edu.in>
2026-03-22 21:55:37 +0530

    Add files via upload

---
+++ Exp-3 Webtechnology and Mobile Application/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException {
+        response.setContentType("text/html");
+        PrintWriter out= response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Hello Servlet</title></head>");
+        out.println("<body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+    }
+}
